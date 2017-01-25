d3016e4e138a00cc802eeacf8bd5a75e8cb8568b
Andy Nguyen <duyanh2912@gmail.com>
2017-01-25 21:23:59 +0700

    Clean up

--- Day06/Database Manager/Pokemon.swift
+++ Day06/Database Manager/Pokemon.swift
@@ -9,13 +9,15 @@
 import Foundation
 
 class Pokemon {
+    var id: Int
     var name: String!
     var tag: String!
     var gen: Int!
     var img: String!
     var color: String!
     
-    init(name: String, tag: String, gen: Int, img: String, color: String) {
+    init(id: Int, name: String, tag: String, gen: Int, img: String, color: String) {
+        self.id = id
         self.name = name
         self.tag = tag
         self.gen = gen
